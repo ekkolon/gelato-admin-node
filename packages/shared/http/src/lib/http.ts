60cb834000b6262569be178e69886b16e3c95462
Nelson Dominguez <18242749+ekkolon@users.noreply.github.com>
2023-02-23 15:32:13 +0100

    feat(http): add `HttpClient` for making authenticated requests

--- packages/shared/http/src/lib/http.ts
+++ packages/shared/http/src/lib/http.ts
@@ -15,6 +15,115 @@
  * limitations under the License.
  */
 
-export function http(): string {
-  return 'http';
+import axios, { AxiosRequestConfig, RawAxiosRequestHeaders } from 'axios';
+
+export const API_KEY_HEADER = 'X-API-KEY';
+export const API_KEY_ENV_VARIABLE = 'GELATO_API_KEY';
+
+/**
+ * Options to customize how a Gelato {@link HttpClient} processes requests.
+ *
+ * @publicApi
+ */
+export interface HttpClientOptions {
+  apiKey?: string;
+}
+
+/**
+ * HTTP client for making (authenticated) requests to Gelato APIs.
+ *
+ * @remarks
+ * All requests to Gelato APIs must include an `X-API-KEY` header.
+ *
+ * You can provide this value in the following ways:
+ *
+ * - Using the `apiKey` option when initializing the client.
+ * - Using an `GELATO_API_KEY` environment variable (recommended).
+ *
+ *    Make sure the current node process contains this variable,
+ *    either by directly assigning it when running your server process
+ *    or by using a _.env_ file and loading it before starting the process.
+ *
+ * @todo Improve response object - provide more context, like statusCode, url.
+ *
+ * @publicApi
+ */
+export class HttpClient {
+  constructor(private readonly options: HttpClientOptions) {}
+
+  /**
+   * Make a `GET` request to an eligable Gelato API endpoint.
+   * @param url Resource URL to fetch.
+   * @param options Options to customize the request.
+   * @returns
+   */
+  async get<T>(url: string, options: AxiosRequestConfig = {}): Promise<T> {
+    const response = await axios.get<T>(url, this._mergeRequestConfig(options));
+
+    return response.data;
+  }
+
+  /**
+   * Make a `POST` request to an eligable Gelato API endpoint.
+   * @param url Target url to request data from.
+   * @param options Options to customize the request.
+   */
+  async post<T, D>(
+    url: string,
+    data: D,
+    options: Omit<AxiosRequestConfig<D>, 'data'> = {},
+  ): Promise<T> {
+    const response = await axios.post<T>(url, data, this._mergeRequestConfig(options));
+
+    return response.data;
+  }
+  /**
+   * Make a `PATCH` request to an eligable Gelato API endpoint.
+   * @param url Resource URL to patch.
+   * @param options Options to customize the request.
+   */
+  async patch<T, D>(url: string, data: D, options: Omit<AxiosRequestConfig<D>, 'data'> = {}) {
+    const response = await axios.patch<T>(url, data, this._mergeRequestConfig(options));
+
+    return response.data;
+  }
+  /**
+   * Make a `DELETE` request to an eligable Gelato API endpoint.
+   * @param url Resource URL to delete.
+   * @param options Options to customize the request.
+   */
+  async delete<T = any>(url: string, options: Omit<AxiosRequestConfig, 'data'> = {}) {
+    const response = await axios.delete<T>(url, this._mergeRequestConfig(options));
+
+    return response.data;
+  }
+
+  private _mergeRequestConfig(options: AxiosRequestConfig) {
+    const { headers, ...requestOptions } = options;
+    return { ...requestOptions, headers: this._mergeRequestHeaders(headers) };
+  }
+
+  private _mergeRequestHeaders(
+    axiosHeaders: AxiosRequestConfig['headers'],
+  ): RawAxiosRequestHeaders {
+    return { ...axiosHeaders, 'Content-Type': 'application/json', ...this._getAuthHeader() };
+  }
+
+  private _getAuthHeader() {
+    const apikKey = this.getApiKey();
+    return { [API_KEY_HEADER]: apikKey };
+  }
+
+  private getApiKey(): string {
+    const apiKey = this.options?.apiKey ?? process.env[API_KEY_ENV_VARIABLE];
+    if (!this.checkApiKeyValue(apiKey)) {
+      throw new Error(`Requests to the Gelato API must use an '${API_KEY_HEADER}' header.`);
+    }
+
+    return apiKey;
+  }
+
+  private checkApiKeyValue(apiKey: unknown): apiKey is string {
+    return apiKey != null && typeof apiKey === 'string' && apiKey.length > 0;
+  }
 }
