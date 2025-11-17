b0073fa5d3ac92a0707de13710e843436eb86ade
Nelson Dominguez <ekkolon@proton.me>
2025-11-17 12:53:47 +0100

    build: update NPM deps to resolve dependabot CVE

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,13 @@
-/* eslint-disable */
-
 export default {
   displayName: 'client',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]sx?$': ['ts-jest', {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    }],
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: './coverage',
